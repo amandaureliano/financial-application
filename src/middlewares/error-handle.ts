689a6210a9b874cef85b7bc393b3b3dc73a10fa7
Amanda Aureliano <a.s.aureliano@hotmail.com>
2023-11-22 22:06:14 -0300

    :recycle: refactor: Pequenas alterações no código

--- src/middlewares/error-handle.ts
+++ src/middlewares/error-handle.ts
@@ -1,8 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 
 export async function errorHandle (err: any, req: Request, res: Response, next: NextFunction) {
+  if (err.message.includes('Unique')) {
+    return res.status(400).send({ message: 'The resource already exists.'})
+  }
+
   if (err.message) {
     return res.status(400).send({ message: err.message })
   }
+
   return res.status(500).send({ message: 'Internal server error.'})
 }
